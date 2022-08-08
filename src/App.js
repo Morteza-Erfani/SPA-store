7568575969421f4a09f63ecd1e5561a5936a69a1
Morteza erfani <m.erfani74@gmail.com>
2022-08-08 22:08:21 +0430

    project starting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
+
+// Styles
+import "./App.scss";
+
+// Components
+import Store from "./components/Store";
+import Navbar from "./components/shared/Navbar";
+import ProductDetails from "./components/ProductDetails";
+import ShopCart from "./components/ShopCart";
+import SignUp from "./components/SignUp";
+
+// Context
+import ProductContextProvider from "./context/ProductContextProvider";
+import CartContextProvider from "./context/CartContextProvider";
+import UserContextProvider from "./context/UserContextProvider";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProductContextProvider>
+        <CartContextProvider>
+          <UserContextProvider>
+            <Navbar />
+            <Routes>
+              <Route path="/products" element={<Store />} />
+              <Route path="/*" element={<Navigate to="/products" />} />
+              <Route path="/products/:id" element={<ProductDetails />} />
+              <Route path="/cart" element={<ShopCart />} />
+              <Route path="/signup" element={<SignUp />} />
+            </Routes>
+          </UserContextProvider>
+        </CartContextProvider>
+      </ProductContextProvider>
     </div>
   );
 }
